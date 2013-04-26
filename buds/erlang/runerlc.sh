df43063cb86054c566b1a3ffa454a6652ba00c25
KDr2 <killy.draw@gmail.com>
2013-04-26 21:12:06 +0800

    sort erlc commands

--- buds/erlang/runerlc.sh
+++ buds/erlang/runerlc.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
 export PATH=$PATH:$HOME/programs/otp/develop/bin
 
-# to beam
-erlc compiler.erl
-
-# to beam with native
-erlc +native compiler.erl
+# to 'P', preprocess
+erlc +"'P'" compiler.erl
 
 # to core erlang
 erlc +to_core compiler.erl
@@ -16,8 +13,11 @@ erlc +"'S'" compiler.erl
 # to 'E'
 erlc +"'E'" compiler.erl
 
-# to 'P'
-erlc +"'P'" compiler.erl
+# to beam
+erlc compiler.erl
+
+# to beam with native
+erlc +native compiler.erl
 
 # disasmbler
 erl -eval 'erts_debug:df(compiler), halt()'
