ab41b566509cc7d4c5098d244bf324429d7525a4
KDr2 <killy.draw@gmail.com>
2013-05-06 16:37:22 +0800

    minor change for llvm sample0

--- badlands/llvm/sample_0.cpp
+++ badlands/llvm/sample_0.cpp
@@ -108,14 +108,14 @@ int main(int argc, char**argv) {
 
 Module* makeLLVMModule() {
     // Module Construction
-    Module* mod = new Module("/tmp/sample1.c", getGlobalContext());
+    Module* mod = new Module("/tmp/sample0.c", getGlobalContext());
     mod->setDataLayout(
         "e-"
         "p:64:64:64-"
         "i1:8:8-i8:8:8-i16:16:16-i32:32:32-i64:64:64-"
         "f32:32:32-f64:64:64-v64:64:64-v128:128:128-"
         "a0:0:64-s0:64:64-f80:128:128-n8:16:32:64-S128");
-    mod->setTargetTriple("x86_64-unknown-linux-gnu");
+    mod->setTargetTriple("x86_64-unknown-linux-gnu"); // arch-vendor-os-env
 
     // Type Definitions
     std::vector<Type*> FuncTy_0_args;
