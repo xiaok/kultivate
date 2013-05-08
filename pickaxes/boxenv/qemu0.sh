d83da193b852b849cb91b54cb34246560d7eff86
KDr2 <killy.draw@gmail.com>
2013-05-08 10:12:06 +0800

    ngx config update and box env setup

---
+++ pickaxes/boxenv/qemu0.sh
@@ -0,0 +1,13 @@
+# Develop Env Setup for QEMU-VM
+
+export SOURCE_ROOT=/data0/source
+export DEST_USR=/data0/usr
+export DEST_LOCAL=/datt0/usr/local
+
+# LuaJIT
+export LUAJIT_SRC=$SOURCE_ROOT/lua
+export LUAJIT_HOME=$DEST_LOCAL/luajit
+
+# Nginx
+export NGX_SRC=$SOURCE_ROOT/nginx
+export NGX_DEST=$DEST_LOCAL/nginx
---
+++ pickaxes/boxenv/x230.sh
@@ -0,0 +1,9 @@
+# Develop Env Setup for X230
+
+# LuaJIT
+export LUAJIT_SRC=$HOME/Work/opensrc/lua
+export LUAJIT_HOME=$HOME/programs/luajit
+
+# Nginx
+export NGX_SRC=$HOME/Work/opensrc/nginx
+export NGX_DEST=$HOME/programs/nginx
