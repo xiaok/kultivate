23ce421bc061c981fe69bdc50d03aa9e79b89767
KDr2 <killy.draw@gmail.com>
2013-06-17 16:49:52 +0800

    env for storm and its deps

---
+++ pickaxes/boxenv/qemu0-storm.sh
@@ -0,0 +1,5 @@
+export ZMQ_SRC=/data0/source/zeromq/zeromq2-x
+export ZMQ_DEST=/data0/usr/local/zeromq
+export ZQM_VERSION=v2.1.7
+export JZMQ_SRC=/data0/source/zeromq/jzmq
+export JZMQ_DEST=/data0/usr/local/zeromq/jzmq
